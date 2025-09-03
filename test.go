766144caf1442e1aff8f5640ab9d164d1a885804
Ilpaka <ilyapanov07@mail.ru>
2025-09-03 16:57:48 +0300

    dfasf

--- test.go
+++ test.go
@@ -1,23 +1,23 @@
-package main
+// package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
+// import (
+// 	"bufio"
+// 	"fmt"
+// 	"os"
+// )
 
-func main() {
-	var message string
-	input_messsage(&message)
-	send_message(&message)
-}
+// func main() {
+// 	var message string
+// 	input_messsage(&message)
+// 	send_message(&message)
+// }
 
-func input_messsage(message *string) {
-	fmt.Println("Введите своё сообщение:")
-	reader := bufio.NewReader(os.Stdin)
-	*message, _ = reader.ReadString('\n')
-}
+// func input_messsage(message *string) {
+// 	fmt.Println("Введите своё сообщение:")
+// 	reader := bufio.NewReader(os.Stdin)
+///// 	*message, _ = reader.ReadString('\n')
+// }
 
-func send_message(message *string) {
-	fmt.Println(*message)
-}
+// func send_message(message *string) {
+// 	fmt.Println(*message)
+// }
